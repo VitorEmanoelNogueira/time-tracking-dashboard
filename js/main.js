96854f14c4e145dccaff9d32b9e7dce15d33fc3a
Vitor Emanoel da Silva Nogueira <vitoremanoelnogueira@gmail.com>
2026-03-18 09:21:15 -0300

    refactor: improve app structure and simplify state handling

--- js/main.js
+++ js/main.js
@@ -4,47 +4,54 @@ let activeButton = document.querySelector('.is-active');
 // spread operator used to turn NodeList into an array and be able to use .map()
 const cards = [...document.querySelectorAll('.c-card')].map((card) => {
     return{
-        current: card.querySelector('.js-current-time'),
-        previous: card.querySelector('.js-previous-time')
+        currentTime: card.querySelector('.js-current-time'),
+        previousTime: card.querySelector('.js-previous-time')
     }
 });
 
-let json;
+let userTimes;
+
+const labels = {
+    daily: "Yesterday",
+    weekly: "Last Week",
+    monthly: "Last Month"
+}
 
 fetch('data.json').then((response) => {
     return response.json();
 }).then((data) => {
-    json = data;
+    userTimes = data;
+    init();
+})
+
+function init(){
     showTime(activeButton.dataset.period);
-})
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        if(button === activeButton) return;
-
-        activeButton.classList.remove('is-active');
-        activeButton.setAttribute("aria-pressed", "false");
-        
-        button.classList.add('is-active');
-        button.setAttribute("aria-pressed", "true");
-        
-        activeButton = button;
-
-        showTime(button.dataset.period);
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            if(button === activeButton) return;
+    
+            updateActiveButton(button)
+        })
     })
-})
+}
 
 function showTime(period){
-    const labels = {
-        daily: "Yesterday",
-        weekly: "Last Week",
-        monthly: "Last Month"
-    }
-
     cards.forEach((card, i) => {
-        const timeframe = json[i].timeframes[period];
+        const timeframe = userTimes[i].timeframes[period];
 
-        card.current.textContent = `${timeframe.current}hrs`;
-        card.previous.textContent = `${labels[period]} - ${timeframe.previous}hrs`
+        card.currentTime.textContent = `${timeframe.current}hrs`;
+        card.previousTime.textContent = `${labels[period]} - ${timeframe.previous}hrs`
     })
+}
+
+function updateActiveButton(button) {
+    activeButton.classList.remove('is-active');
+    activeButton.setAttribute("aria-pressed", "false");
+    
+    button.classList.add('is-active');
+    button.setAttribute("aria-pressed", "true");
+    
+    activeButton = button;
+    showTime(activeButton.dataset.period);
 }
